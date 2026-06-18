de7e3a50abb6bc2a20281a107fa89b4e053ee742
hartunnoo <hartunnoo@gmail.com>
2026-06-18 00:48:06 +0000

    fix: remove duplicate source files — clean project structure

---
+++ src/PrayerTimeV1.Maui/obj/Release/net10.0-android/android/src/crc6439eaa43dbe0ff54e/MainActivity.java
@@ -0,0 +1,46 @@
+package crc6439eaa43dbe0ff54e;
+
+
+public class MainActivity
+	extends crc6488302ad6e9e4df1a.MauiAppCompatActivity
+	implements
+		mono.android.IGCUserPeer
+{
+/** @hide */
+	public static final String __md_methods;
+	static {
+		__md_methods = 
+			"";
+		mono.android.Runtime.register ("PrayerTimeV1.Maui.MainActivity, PrayerTimeV1.Maui", MainActivity.class, __md_methods);
+	}
+
+	public MainActivity ()
+	{
+		super ();
+		if (getClass () == MainActivity.class) {
+			mono.android.TypeManager.Activate ("PrayerTimeV1.Maui.MainActivity, PrayerTimeV1.Maui", "", this, new java.lang.Object[] {  });
+		}
+	}
+
+	public MainActivity (int p0)
+	{
+		super (p0);
+		if (getClass () == MainActivity.class) {
+			mono.android.TypeManager.Activate ("PrayerTimeV1.Maui.MainActivity, PrayerTimeV1.Maui", "System.Int32, System.Private.CoreLib", this, new java.lang.Object[] { p0 });
+		}
+	}
+
+	private java.util.ArrayList refList;
+	public void monodroidAddReference (java.lang.Object obj)
+	{
+		if (refList == null)
+			refList = new java.util.ArrayList ();
+		refList.add (obj);
+	}
+
+	public void monodroidClearReferences ()
+	{
+		if (refList != null)
+			refList.clear ();
+	}
+}
